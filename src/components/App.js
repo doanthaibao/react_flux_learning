8796df2fe306aa46e430719d0eb696a451d5a061
Bao Doan <doanthaibao@gmail.com>
2019-08-21 23:31:16 +0700

    Switch and 404

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import HomePage from "./HomePage";
 import AboutPage from "./About";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
-import { Route } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/courses" component={CoursesPage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />\
+      </Switch>
     </div>
   );
 }
